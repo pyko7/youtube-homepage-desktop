285346ce76c99cb8cf4fbac433048ce3da9fc69e
julien henry <julienhenry967@gmail.com>
2023-05-10 17:58:35 +0200

    Updates

--- src/components/Chips/VideoCardChips.tsx
+++
@@ -1,5 +0,0 @@
-const VideoCardChips = () => {
-  return <span className="w-fit px-2 bg-main-bg/70 rounded-md text-xs tracking-wide">23:14</span>;
-};
-
-export default VideoCardChips;
--- src/components/Navigation/NavigationMenuList.tsx
+++
@@ -1,7 +0,0 @@
-import { Children } from "../../types/types";
-
-const NavigationMenuList = ({ children }: Children) => {
-  return <ul className="w-full flex flex-col">{children}</ul>;
-};
-
-export default NavigationMenuList;
